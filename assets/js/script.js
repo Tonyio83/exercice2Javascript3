e7fe8106447e50f28c5dbbc2165149e4a9c3640e
Tonyio83 <anthonyfrancelle@gmail.com>
2019-10-29 13:51:37 +0100

    exercice 2 correction

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let display = document.getElementById('display');
+let hide = document.getElementById('hide');
+let text = document.getElementById('text');
+
+display.onclick = function(){
+  text.style.visibility = 'visible'
+}
+hide.onclick = function(){
+  text.style.visibility = 'hidden'
+}
